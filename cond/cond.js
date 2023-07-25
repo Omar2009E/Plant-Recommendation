c24919f1c661612adcef7cd64ecf7d4a2844d303
Omar2009E <123904433+Omar2009E@users.noreply.github.com>
2023-07-25 16:36:38 +0400

    Add files via upload

---
+++ cond/cond.js
@@ -0,0 +1,13 @@
+const wheather="sunny";
+const soil="clay";
+const resources="limited";
+const water1="1";
+const water2="2";
+
+if (wheather, soil, resources, water1, water2){
+    console.log('Recommended plant is Palm')
+}
+else{
+    console.log('Recommended is not Palm')
+
+}
